a7a3a98fb12593780436fe42099dbe10178c6027
Bruno Ramos <aventuraland23@gmail.com>
2026-01-14 20:24:50 +0000

    chore(name): rename all the project

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,16 @@
-import { Helmet } from 'react-helmet';
+import { Helmet } from "react-helmet";
 
 export function LoginPage() {
-  return (
-    <>
-      <Helmet>
-        <title>Track Verse | Login</title>
-      </Helmet>
-              
-      <main className="flex h-screen flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-600">Login Page</h1>
-        <p className="text-sm text-blue-500">Please log in to continue.</p>
-      </main>
-    </>
-  )
+	return (
+		<>
+			<Helmet>
+				<title>TrackGeek | Login</title>
+			</Helmet>
+
+			<main className="flex h-screen flex-col items-center justify-center">
+				<h1 className="text-4xl font-bold text-blue-600">Login Page</h1>
+				<p className="text-sm text-blue-500">Please log in to continue.</p>
+			</main>
+		</>
+	);
 }
