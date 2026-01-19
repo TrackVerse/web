d2cd82de43fbc34612d60f30a6d2609b6299871b
Bruno Ramos <aventuraland23@gmail.com>
2026-01-19 16:39:00 +0000

    feat(footer): make the footer

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -42,7 +42,38 @@ export function Layout({ title, children }: LayoutProps) {
 						</Button>
 					</div>
 				</header>
-				<main className="flex flex-col px-5 py-5">{children}</main>
+				<main className="flex flex-col grow px-5 py-5">{children}</main>
+				<footer className="bg-card border-t border-border w-full px-10 py-6">
+					<div className="flex flex-col lg:flex-row items-center justify-between gap-8">
+						<div className="flex flex-col items-center justify-center gap-3">
+							<Link to="/">
+								<img src="/logo.svg" alt="Logo" className="h-full w-45" />
+							</Link>
+						</div>
+						<div className="flex flex-row gap-8 lg:gap-12 justify-between w-1/2">
+							<div className="flex flex-col gap-3">
+								<h3 className="text-accent font-bold">{t("common:community")}</h3>
+								<Link to="/">{t("common:addData")}</Link>
+								<Link to="/">{t("common:about")}</Link>
+								<Link to="/">{t("common:become")} Premium</Link>
+								<Link to="/">{t("common:community")}</Link>
+							</div>
+							<div className="flex flex-col gap-3">
+								<h3 className="text-accent font-bold">{t("common:legal")}</h3>
+								<Link to="/">{t("common:terms")}</Link>
+								<Link to="/">{t("common:privacy")}</Link>
+							</div>
+							<div className="flex flex-col gap-3">
+								<h3 className="text-accent font-bold">{t("common:socials")}</h3>
+								<Link to="/">BlueSky</Link>
+								<Link to="/">Discord</Link>
+								<Link to="/">Instagram</Link>
+								<Link to="/">Twitter</Link>
+								<Link to="/">GitHub</Link>
+							</div>
+						</div>
+					</div>
+				</footer>
 			</div>
 		</>
 	);
