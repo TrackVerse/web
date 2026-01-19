985f2bed80d28d9328f071236e200444bbbfff3b
Bruno Ramos <aventuraland23@gmail.com>
2026-01-19 18:02:41 +0000

    feat(...): many things will come

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -10,16 +10,26 @@ interface LayoutProps {
 }
 
 export function Layout({ title, children }: LayoutProps) {
-	const { t, i18n } = useTranslation();
+	const { t } = useTranslation();
 
 	return (
 		<>
 			<Helmet>
 				<title>{title} | TrackGeek</title>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin=""
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&display=swap"
+					rel="stylesheet"
+				></link>
 			</Helmet>
 
 			<div className="bg-background flex flex-col min-h-screen">
-				<header className="bg-card border-b border-border w-full h-14 flex items-center justify-between px-5 py-2">
+				<header className="bg-border/30 backdrop-blur border-b border-border w-full h-14 flex items-center justify-between px-5 py-2">
 					<div className="flex items-center justify-center gap-3">
 						<Button>
 							<Menu />
@@ -29,21 +39,23 @@ export function Layout({ title, children }: LayoutProps) {
 						</Link>
 					</div>
 					<div className="flex items-center justify-center gap-3">
-						<Button variant={"outline"} className="">
-							<Heart color="red" fill="red" />
-							{t("common:donate")}
-						</Button>
+						<Link to="/donate">
+							<Button variant={"outline"} className="">
+								<Heart color="red" fill="red" />
+								{t("common:donate")}
+							</Button>
+						</Link>
 						<Button>
 							<Bell fill="" />
 						</Button>
-						<Button>
+						<Button className="font-semibold">
 							<LogIn />
 							{t("auth:login")}
 						</Button>
 					</div>
 				</header>
 				<main className="flex flex-col grow px-5 py-5">{children}</main>
-				<footer className="bg-card border-t border-border w-full px-10 py-6">
+				<footer className="bg-border/30 backdrop-blur border-t border-border w-full px-10 py-6">
 					<div className="flex flex-col lg:flex-row items-center justify-between gap-8">
 						<div className="flex flex-col items-center justify-center gap-3">
 							<Link to="/">
@@ -52,10 +64,12 @@ export function Layout({ title, children }: LayoutProps) {
 						</div>
 						<div className="flex flex-row gap-8 lg:gap-12 justify-between w-1/2">
 							<div className="flex flex-col gap-3">
-								<h3 className="text-accent font-bold">{t("common:community")}</h3>
+								<h3 className="text-accent font-bold">
+									{t("common:community")}
+								</h3>
 								<Link to="/">{t("common:addData")}</Link>
 								<Link to="/">{t("common:about")}</Link>
-								<Link to="/">{t("common:become")} Premium</Link>
+								<Link to="/">{t("common:donate")}</Link>
 								<Link to="/">{t("common:community")}</Link>
 							</div>
 							<div className="flex flex-col gap-3">
@@ -65,11 +79,21 @@ export function Layout({ title, children }: LayoutProps) {
 							</div>
 							<div className="flex flex-col gap-3">
 								<h3 className="text-accent font-bold">{t("common:socials")}</h3>
-								<Link to="/">BlueSky</Link>
-								<Link to="/">Discord</Link>
-								<Link to="/">Instagram</Link>
-								<Link to="/">Twitter</Link>
-								<Link to="/">GitHub</Link>
+								<Link to="/" target="_blank">
+									BlueSky
+								</Link>
+								<Link to="https://discord.gg/76bcftRnuT" target="_blank">
+									Discord
+								</Link>
+								<Link to="/" target="_blank">
+									Instagram
+								</Link>
+								<Link to="/" target="_blank">
+									Twitter
+								</Link>
+								<Link to="https://github.com/TrackGeek/" target="_blank">
+									GitHub
+								</Link>
 							</div>
 						</div>
 					</div>
