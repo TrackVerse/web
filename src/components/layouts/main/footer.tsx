9a328b1c3c716e5fab4c968ffed48700200ab9bb
Bruno Ramos <aventuraland23@gmail.com>
2026-01-31 17:48:38 +0000

    feat(footer): add credits, and metadata providers

--- src/components/layouts/main/footer.tsx
+++ src/components/layouts/main/footer.tsx
@@ -1,4 +1,9 @@
-import { useTranslation } from "react-i18next";
+import {
+	SiIgdb,
+	SiMyanimelist,
+	SiThemoviedatabase,
+} from "@icons-pack/react-simple-icons";
+import { Trans, useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 export function Footer() {
@@ -7,10 +12,43 @@ export function Footer() {
 	return (
 		<footer className="bg-border/30 backdrop-blur border-t border-border w-full px-3 md:px-10 py-6">
 			<div className="flex flex-col lg:flex-row md:items-center justify-between gap-8">
-				<div className="flex flex-col items-center justify-center gap-3">
+				<div className="flex flex-col justify-center gap-3">
 					<Link to="/">
 						<img src="/logo.svg" alt="Logo" className="h-full w-45" />
 					</Link>
+					<p className="flex gap-1 items-center">
+						{t("common:providers.IGDBMetadata")}
+						<Link to="https://www.igdb.com" target="_blank">
+							<SiIgdb />
+						</Link>
+					</p>
+					<p className="flex gap-1 items-center">
+						{t("common:providers.TMDBMetadata")}
+						<Link to="https://www.themoviedb.org" target="_blank">
+							<SiThemoviedatabase />
+						</Link>
+					</p>
+					<p className="flex gap-1 items-center">
+						<Trans
+							i18nKey="common:providers.HardCoverMetadata"
+							components={{
+								1: <Link to="https://hardcover.app" target="_blank" />,
+							}}
+						/>
+					</p>
+					<p className="flex gap-1 items-center">
+						<Trans
+							i18nKey="common:providers.MALMetadata"
+							components={{
+								mal: (
+									<Link to="https://myanimelist.net" target="_blank">
+										<SiMyanimelist />
+									</Link>
+								),
+								jikan: <Link to="https://jikan.moe" target="_blank" />,
+							}}
+						/>
+					</p>
 				</div>
 
 				<div className="flex max-sm:flex-col gap-8 lg:gap-12 justify-between w-1/2">
@@ -20,6 +58,7 @@ export function Footer() {
 						<Link to="/add-data">{t("common:addData")}</Link>
 						<Link to="/about">{t("common:about")}</Link>
 						<Link to="/donate">{t("common:donate")}</Link>
+						<Link to="/special-thanks">{t("common:credits")}</Link>
 						<Link to="https://crowdin.com/project/trackgeek" target="_blank">
 							{t("common:translate")}
 						</Link>
